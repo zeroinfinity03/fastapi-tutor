91aec846b463613e067f4a4d806b7c83b6a4a101
zeroinfinity03 <zeroinfinity03@gmail.com>
2025-11-20 12:13:34 -0500

    first commit

--- 5.2 ML endpoints/predict.py
+++ 5.2 ML endpoints/predict.py
@@ -7,10 +7,12 @@ print('Loaded the Model')
 
 
 # users se data json me aayega and when it comes to this fn call, 
-# we will get it as a dict and this will return a floting value the actual predicted value.
+# we will get it as a dict, we use something like user_input.model_dump() in main.py to convert json into dict.
+# and this will return a floting value the actual predicted value.
 # also our model expects a numpy 2d array as input to make predictions.
+
 # json -> dict -> numpy 2d array -> model prediction -> float
-# we use something like user_input.model_dump() in main.py to convert json into dict.
+
 
 def make_prediction(data: dict) -> float:
     features = np.array([
@@ -32,6 +34,7 @@ def make_prediction(data: dict) -> float:
 
 # sckit learn returns prediction in this format: array([ 452600.55, 4524322.45, 45242.32 ])
 # but since we will send only 1 record all the time, we will get only 1 value in the array:  array([ 452600.55]) => so we do [0] to get the first value from the array.
+
 
 
 
