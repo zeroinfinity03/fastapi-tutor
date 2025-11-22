07fef12f079613e97717c331b98e6d77eeb12a04
zeroinfinity03 <zeroinfinity03@gmail.com>
2025-11-21 19:05:39 -0500

    first commit

--- 6. Advanced FastAPI Concepts/middleware/cors-middleware.py
+++ 6. Advanced FastAPI Concepts/middleware/cors-middleware.py
@@ -1,10 +1,7 @@
 
-
 # cors se we can allow or disallow requests from different origins (domains)
 # by default, browsers block requests from different origins for security reasons.
 # but with CORS middleware, we can specify which origins are allowed to access our API.
-
-
 
 
 from fastapi import FastAPI
@@ -14,9 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        'https://my-frontend.com', 'http://localhost:3000'
-    ],
+    allow_origins=['https://my-frontend.com', 'http://localhost:3000'],
     allow_credentials=True,
     allow_methods=['GET', 'POST', 'PUT', 'DELETE'],
     allow_headers=['*']
