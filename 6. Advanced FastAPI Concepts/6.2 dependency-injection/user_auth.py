cb79728cab4b1233025044771f16e9451a3acfa5
zeroinfinity03 <zeroinfinity03@gmail.com>
2025-11-27 18:15:53 -0500

    first commit

--- 6. Advanced FastAPI Concepts/6.2 dependency-injection/user_auth.py
+++ 6. Advanced FastAPI Concepts/6.2 dependency-injection/user_auth.py
@@ -5,8 +5,20 @@ app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 
+# Step1:
+# user sends userid and password, token endpoint will be used,
+# and in this endpoint we will verify the user_id and password if its Correct then we will give back a token else raise error.
+
+
+# Step2:
+# user want to access the profile, profile end point will used,
+# and in this endpoint we will accept the token that was generated in step1, and if its a valid token then we will give back the profile info else raise error.
+
+
+
+
 @app.post('/token')
-def login(username: str = Form(...), password: str = Form(...)):
+def login(username: str = Form(...), password: str = Form(...)):     # required value will be specifies via(...)
     if username == 'john' and password == 'pass123':
         return {'access_token': 'valid_token', 'token_type': 'bearer'}
     raise HTTPException(status_code=400, detail='Invalid Credentials')
@@ -21,10 +33,35 @@ def decode_token(token: str):
     )
 
 
+
+# to get the user we need the valid token which depends on oauth2_scheme.
+# this line that we have written will give the token:    oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+# and this token we need to decode.
 def get_current_user(token: str = Depends(oauth2_scheme)):
     return decode_token(token)
 
 
+
+# this endpoint will just give back the user details.
 @app.get('/profile')
 def get_profile(user=Depends(get_current_user)):
     return {'username': user['name']}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
